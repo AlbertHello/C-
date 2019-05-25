b6b656bd457cb211a5fd363d26dd247ffa0072c0
AlbertHello <815878037@qq.com>
2019-05-25 19:59:06 +0800

    改动

---
+++ 8、命名空间/命名空间/命名空间/Person.hpp
@@ -0,0 +1,25 @@
+//
+//  Person.hpp
+//  声明和实现的分离
+//
+//  Created by 58 on 2019/5/25.
+//  Copyright © 2019 58. All rights reserved.
+//
+
+#ifndef Person_hpp
+#define Person_hpp
+
+#include <stdio.h>
+namespace Albert {
+    //声明 放在头文件
+    class Person{
+        int m_age;
+    public:
+        Person();
+        ~Person();
+        void setAge(int age);
+        int getAge();
+    };
+}
+
+#endif /* Person_hpp */
