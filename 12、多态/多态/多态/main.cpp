04600a55df4490795c4afefc968150c40223c77b
AlbertHello <815878037@qq.com>
2019-06-15 16:03:48 +0800

    多态、虚函数的学习

--- 12、多态/多态/多态/main.cpp
+++ 12、多态/多态/多态/main.cpp
@@ -11,7 +11,12 @@ using namespace std;
 
 class Animal{
 public:
-    void run(){
+    //C++中的多态通过虚函数(virtual function)来实现
+//    virtual void run(){
+//        cout << "Animal:run()" <<endl;
+//    }
+    
+     void run(){
         cout << "Animal:run()" <<endl;
     }
 };
@@ -26,13 +31,57 @@ public:
 
 class Dog: public Animal{
 public:
-    void run(){
+//只要在父类中声明为虚函数，子类中重写的函数也自动变成虚函数(也就是说子类中可以省略virtual关键字)
+//    void run(){
+//        cout << "Dog:run()" <<endl;
+//    }
+    virtual void run(){
         cout << "Dog:run()" <<endl;
     }
 };
 
+
+class ErHa: public Dog{
+public:
+    void run(){
+        cout << "ErHa:run()" <<endl;
+    }
+};
+
+void liu(Animal *animal){
+    animal->run();
+}
+
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    
+    // 虚函数关键字写在Animal中
+//    Animal *animal0=new Cat();
+//    Animal *animal1=new Dog();
+//    Animal *animal2=new Animal();
+//    animal0->run();
+//    animal1->run();
+//    animal2->run();
+    
+    //虚函数关键字写在Dog中。此时多台就不能完成。
+//    Animal *animal1=new Animal();
+//    Animal *animal2=new Dog();
+//    Animal *animal3=new ErHa();
+//    animal1->run();
+//    animal2->run();
+//    animal3->run();
+    
+    //virtual关键字写在Dog *dog1指针类型所在类中，多态才能起作用
+    Dog *dog1=new Dog();
+    Dog *dog2=new ErHa();
+    dog1->run();
+    dog2->run();
+    
+    
+    
+    
+    
+    
     return 0;
 }
